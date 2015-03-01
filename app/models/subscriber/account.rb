817a206c52c89327d09cd6e19c28582c1466dd90
Abdul Miah <momun20@gmail.com>
2015-03-01 16:21:20 -0600

    master restrict dirty subdomains

--- app/models/subscriber/account.rb
+++ app/models/subscriber/account.rb
@@ -1,7 +1,16 @@
 module Subscriber
   class Account < ActiveRecord::Base
-    validates :subdomain, :presence => true, :uniqueness => true
+    before_validation do
+      self.subdomain = subdomain.to_s.downcase
+    end
 
+    EXCLUDED_SUBDOMAINS = %w(admin)
+    
+    validates :subdomain, :presence => true, :uniqueness => true
+    validates_exclusion_of :subdomain, :in => EXCLUDED_SUBDOMAINS,
+                          :message => "is not allowed. Please choose another subdomain."
+    validates_format_of :subdomain, :with => /\A[\w\-]+\Z/i,
+                        :message => "is not allowed. Please choose another subdomain."
     belongs_to :owner, :class_name => "Subscriber::User"
     
     accepts_nested_attributes_for :owner
