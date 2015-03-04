f288680c8ab8305ef5f41d5e883becfda247868e
Abdul Miah <momun20@gmail.com>
2015-03-04 14:09:40 -0600

    master mailer test

--- spec/mailers/subscriber/user_mailer_spec.rb
+++ spec/mailers/subscriber/user_mailer_spec.rb
@@ -1,20 +1,21 @@
 require "rails_helper"
-
+require "subscriber/testing_support/factories/user_factory"
 module Subscriber
-  # RSpec.describe UserMailer, :type => :mailer do
-  #   describe "password_reset" do
-  #     let(:mail) { UserMailer.password_reset }
+  RSpec.describe UserMailer, :type => :mailer do
+    let!(:user) { FactoryGirl.create(:user) }
+    describe "password_reset" do
+      let(:mail) { UserMailer.password_reset(user) }
 
-  #     it "renders the headers" do
-  #       expect(mail.subject).to eq("Password reset")
-  #       expect(mail.to).to eq(["to@example.org"])
-  #       expect(mail.from).to eq(["from@example.com"])
-  #     end
+      it "renders the headers" do
+        expect(mail.subject).to eq("Password Reset")
+        expect(mail.to).to eq([user.email])
+        expect(mail.from).to eq(["from@example.com"])
+      end
 
-  #     it "renders the body" do
-  #       expect(mail.body.encoded).to match("Hi")
-  #     end
-  #   end
+      it "renders the body" do
+        expect(mail.body.encoded).to match("To reset your password, click the URL below.")
+      end
+    end
 
-  # end
+  end
 end
