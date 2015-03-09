99f62afa9feb7d4a6292603b346f9e5139a8613b
Abdul Miah <momun20@gmail.com>
2015-03-09 11:44:35 -0500

    master remove meta type from user

--- db/migrate/20150309163408_add_meta_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddMetaToUsers < ActiveRecord::Migration
-  def change
-    add_column :subscriber_users, :meta_id, :integer
-    add_column :subscriber_users, :meta_type, :string
-
-    add_index :subscriber_users, [:meta_id, :meta_type]
-  end
-end
