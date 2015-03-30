71fab6bb7ca7c8b85a4b2b4d3c3511a601a130da
Abdul Miah <momun20@gmail.com>
2015-03-30 18:20:36 -0500

    master comment out account signup path

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,6 @@ Subscriber::Engine.routes.draw do
   end
 
   root "dashboard#index"
-  get "/sign_up", :to => "accounts#new", :as => :sign_up
-  post "/accounts", :to => "accounts#create", :as => :accounts
+  # get "/sign_up", :to => "accounts#new", :as => :sign_up
+  # post "/accounts", :to => "accounts#create", :as => :accounts
 end
